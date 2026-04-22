32fb65ee327b55035fa601df5fc03a349344ec13
ZephyrC <zephyr724@gmail.com>
2026-04-22 19:16:56 +1200

    4 api -5 update an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -12,3 +12,19 @@ export async function GET(
 
   return NextResponse.json({ id: 1, name: "zephyr" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const body = await request.json();
+  const { id } = await params;
+  const idNumber = parseInt(id);
+
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required." }, { status: 400 });
+
+  if (idNumber > 10)
+    return NextResponse.json({ error: "Name is not found." }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+}
