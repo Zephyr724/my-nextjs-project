acb4a83373de4863d532df167bec695faecda5d0
ZephyrC <zephyr724@gmail.com>
2026-04-22 19:25:48 +1200

    4 api -6 Delete an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -28,3 +28,17 @@ export async function PUT(
     return NextResponse.json({ error: "Name is not found." }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const body = await request.json();
+  const { id } = await params;
+  const idNumber = parseInt(id);
+
+  if (idNumber > 10)
+    return NextResponse.json({ error: "User not found." }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+}
